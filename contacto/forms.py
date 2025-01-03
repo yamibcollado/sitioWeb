4f6467d82f8f44e8dbcba4f4c7fa5cf276469f6f
Yami <yamicollado@hotmail.com>
2025-01-03 00:19:25 -0300

    final de la app contacto

--- contacto/forms.py
+++ contacto/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class Formulario(forms.Form):
     nombre=forms.CharField(label="nombre", required=True)
     email=forms.CharField(label="email", required=True)
-    contenido=forms.CharField(label="contenido", widget=forms.Textarea)
+    mensaje=forms.CharField(label="mensaje", widget=forms.Textarea)
--- contacto/views.py
+++ contacto/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import Formulario
 
 def contacto(request):
     formulario_contacto=Formulario()
-    return render(request, "contacto/contacto.html", {'formulario':formulario_contacto})
 
+    if request.method=="POST":
+        formulario_contacto=Formulario(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre=request.POST.get("nombre")
+            email=request.POST.get("email")
+            mensaje=request.POST.get("mensaje")
+            
+            return redirect("/contacto/?valido")
+
+    return render(request, "contacto/contacto.html", {'formulario':formulario_contacto})
 
